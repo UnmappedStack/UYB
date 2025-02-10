2049aa04bd6160a5abcb92390ceb4445e7f8f8e3
UnmappedStack <jake.steinburger0@gmail.com>
2025-02-11 09:57:27 +1100

    whitespace handling in lexer

--- src/lexer.c
+++ src/lexer.c
@@ -22,7 +22,8 @@ Token **lex_line(char *str, size_t line_num) {
     Token **ret = vec_new(sizeof(Token));
     size_t len = strlen(str);
     for (size_t i = 0; i < len; i++) {
-        if      (str[i] == '(') vec_push(ret, ((Token) {.line=line_num,.type=TokLParen,.val=0}));
+        if      (str[i] == '\t' || str[i] == ' ') continue;
+        else if (str[i] == '(') vec_push(ret, ((Token) {.line=line_num,.type=TokLParen,.val=0}));
         else if (str[i] == ')') vec_push(ret, ((Token) {.line=line_num,.type=TokRParen,.val=0}));
         else if (str[i] == '{') vec_push(ret, ((Token) {.line=line_num,.type=TokLBrace,.val=0}));
         else if (str[i] == '}') vec_push(ret, ((Token) {.line=line_num,.type=TokRBrace,.val=0}));
