c129e13ae13f6763c5576e9c435482970205a614
UnmappedStack <jake.steinburger0@gmail.com>
2025-02-12 11:58:30 +1100

    do some checks

--- src/main.c
+++ src/main.c
@@ -25,66 +25,12 @@ int main() {
     fclose(inf);
     Function **functs = parse_program(toks);
     FILE *outf = fopen("out.S", "w");
+    if (!outf) {
+        printf("Failed to open out.S\n");
+        exit(1);
+    }
     printf("size = %zu\n", vec_size(functs));
     build_program(*functs, vec_size(functs), globals, sizeof(globals) / sizeof(globals[0]), outf);
     fclose(outf);
     return 0;
-    /* ### CODEGEN TEST ### */
-    // Define globals
-    // Define Function list
-    FunctionArgList *argument_vals = malloc(sizeof(FunctionArgList));
-    char *args[] = {"msg"};
-    *argument_vals = (FunctionArgList) {
-        .args = args,
-        .num_args = 1,
-    };
-    Function IR[] = {
-        (Function) {
-            .is_global = true,
-            .name = "main",
-            .num_args = 0,
-            .return_type = Bits64,
-            .statements = (Statement[]) {
-                (Statement) {
-                    .label = "val1",
-                    .instruction = COPY,
-                    .type = Bits32,
-                    .vals = {4},
-                    .val_types = {Number, Empty, Empty},
-                },
-                (Statement) {
-                    .label = "val2",
-                    .instruction = COPY,
-                    .type = Bits32,
-                    .vals = {2},
-                    .val_types = {Number, Empty, Empty},
-                },
-                (Statement) {
-                    .label = "ret",
-                    .instruction = DIV,
-                    .type = Bits32,
-                    .vals = {(uint64_t) "val1", (uint64_t) "val2"},
-                    .val_types = {Label, Label, Empty},
-                },
-                (Statement) {
-                    .label = "ret",
-                    .instruction = EXT,
-                    .type = Bits64,
-                    .vals = {(uint64_t) "ret", false},
-                    .val_types = {Label, Number, Empty},
-                },
-                (Statement) {
-                    .label = NULL, // it doesn't save the result in any label
-                    .instruction = RET,
-                    .type = None, // type not specified since it's not saving a value in a label
-                    .vals = {(uint64_t) "ret.1", 0},
-                    .val_types = {Label, Empty, Empty},
-                },
-            },
-            .num_statements = 5,
-        },
-    };
-    FILE *f = fopen("out.S", "w");
-    build_program(IR, sizeof(IR) / sizeof(IR[0]), globals, sizeof(globals) / sizeof(globals[0]), f);
-    fclose(f);
 }
