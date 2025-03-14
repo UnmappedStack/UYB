3aa254509c4bcc18bb536ddb3b1b84ea8f7d3c81
UnmappedStack <jake.steinburger0@gmail.com>
2025-03-14 18:03:58 +1100

    remove -OX flag todo

--- src/optimise/optimisation.c
+++ src/optimise/optimisation.c
@@ -3,8 +3,7 @@
 /* Takes a pointer to an array of Function structures and the number of functions in the IR.
  * Changes the statements in the given function to be more optimised. */
 void optimise(Function *IR, size_t num_functions) {
-    /* TODO: Change which optimisations are done based on -OX flag.
-     * Planned optimisations:
+    /* Planned optimisations:
      *  - Folding [DONE]
      *  - Copy elimination [DONE]
      *  - Unused label removal [DONE]
